f3e36337ab8c0e683742bbad7e5f4478cadd9ffb
ktschroeder <43946531+ktschroeder@users.noreply.github.com>
2021-07-26 02:57:42 -0500

    Semi-initial commit
    basic map; grid player movement; function to determine tile from grid location. Controls: WASD/E

---
+++ Rhythm Jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool isMoving;
+    private Vector3 origPos, targetPos;
+    private float timeToMove = 0.2f; // in seconds
+    private Vector3 gridPos = new Vector3(1,1,0); // game-starting grid position. TODO this is magic and not directly tied to the player if they are dragged in the editor
+
+    public TileBase tileA;
+    public TileBase tileB;
+    public Vector2Int size;
+    public Tilemap tilemap;
+    public TileController tileController;
+
+    void Update(){
+        if(Input.GetKey(KeyCode.W) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.up));
+        if(Input.GetKey(KeyCode.A) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.left));
+        if(Input.GetKey(KeyCode.S) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.down));
+        if(Input.GetKey(KeyCode.D) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.right));
+            
+        if(Input.GetKeyDown(KeyCode.E)){
+            int playerNearGridX = (int)Math.Round(transform.position.x - 0.5f); // the -0.5f is because the sprite position is determined by its center, whereas the tilemap tiles are determined by their lower left corner
+            int playerNearGridY = (int)Math.Round(transform.position.y - 0.5f); // this rounding lets a player perform an action on a grid spot while moving
+            tileController.TestSwap(new Vector3Int(playerNearGridX, playerNearGridY, 0));
+        }
+
+    }
+
+    private IEnumerator MovePlayer(Vector3 direction){
+        isMoving = true;
+
+        float elapsedTime = 0;
+        origPos = transform.position;
+        targetPos = origPos + direction;
+
+        while(elapsedTime < timeToMove){
+            transform.position = Vector3.Lerp(origPos, targetPos, elapsedTime / timeToMove);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos; // this prevents any accumulation of imperfect movements
+
+        isMoving = false;
+    }
+}
---
+++ Rhythm Jam/Assets/Scripts/TileController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileController : MonoBehaviour
+{
+    private Tilemap tilemap;
+    public TileBase grass;
+    public TileBase plantspot; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = gameObject.GetComponent<Tilemap>();
+    }
+
+    // returns the name of the tile's sprite at this position, e.g. "grass"
+    public string GetTileName(Vector3Int pos){
+        if(tilemap == null)
+            return null;
+
+        // TODO check position is valid
+        
+        return tilemap.GetTile<Tile>(pos).name;
+    }
+
+    // toggles a spot. This is only meant for testing purposes
+    public void TestSwap(Vector3Int pos){
+        var name = tilemap.GetTile<Tile>(pos).name;
+        if(name == "grass"){
+            tilemap.SetTile(pos, plantspot);
+        }
+        else tilemap.SetTile(pos, grass);
+    }
+}
