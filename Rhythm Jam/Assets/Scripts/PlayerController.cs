60515859e00dbe68feb3e8946ab7176f7f7ca08e
ktschroeder <43946531+ktschroeder@users.noreply.github.com>
2021-07-30 00:56:45 -0500

    Planting, uprooting, art assets, moving animation

--- Rhythm Jam/Assets/Scripts/PlayerController.cs
+++ Rhythm Jam/Assets/Scripts/PlayerController.cs
@@ -10,26 +10,56 @@ public class PlayerController : MonoBehaviour
     private Vector3 origPos, targetPos;
     private float timeToMove = 0.2f; // in seconds
     private Vector3 gridPos = new Vector3(1,1,0); // game-starting grid position. This is magic and not directly tied to the player if they are dragged in the editor
-    
+    private bool rightFootNext = true;
+    private InstrumentEnum.Instrument instrumentSelection = InstrumentEnum.Instrument.Piano;
+
     [FMODUnity.EventRef]
 	public string FootstepsEvent = "";
 
     public TileController tileController;
 
+    public Sprite[] spriteArray; // First 4 are downward. Next 4 are upward. Next 2 are leftward. Last 2 are rightward.
+    public SpriteRenderer spriteRenderer;
+
+
+    void Start(){
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
     void Update(){
-        if(Input.GetKey(KeyCode.W) && !isMoving && transform.position.y <= tileController.gridHeight - 1)
+        if(Input.GetKey(KeyCode.W) && !isMoving && transform.position.y <= tileController.gridHeight - 0)
             StartCoroutine(MovePlayer(Vector3.up));
         if(Input.GetKey(KeyCode.A) && !isMoving && transform.position.x >= 1)
             StartCoroutine(MovePlayer(Vector3.left));
-        if(Input.GetKey(KeyCode.S) && !isMoving && transform.position.y >= 1)
+        if(Input.GetKey(KeyCode.S) && !isMoving && transform.position.y >= 2)
             StartCoroutine(MovePlayer(Vector3.down));
         if(Input.GetKey(KeyCode.D) && !isMoving && transform.position.x <= tileController.gridLength - 1)
             StartCoroutine(MovePlayer(Vector3.right));
+
+        if(Input.GetKey(KeyCode.Alpha1))
+            instrumentSelection = InstrumentEnum.Instrument.Piano;
+        if(Input.GetKey(KeyCode.Alpha2))
+            instrumentSelection = InstrumentEnum.Instrument.Organ;
+        if(Input.GetKey(KeyCode.Alpha3))
+            instrumentSelection = InstrumentEnum.Instrument.Guitar;
+        if(Input.GetKey(KeyCode.Alpha4))
+            instrumentSelection = InstrumentEnum.Instrument.Drums;
+        if(Input.GetKey(KeyCode.Alpha5))
+            instrumentSelection = InstrumentEnum.Instrument.Bass;
             
         if(Input.GetKeyDown(KeyCode.E)){
             int playerNearGridX = (int)Math.Round(transform.position.x - 0.5f); // the -0.5f is because the sprite position is determined by its center, whereas the tilemap tiles are determined by their lower left corner
-            int playerNearGridY = (int)Math.Round(transform.position.y - 0.5f); // this rounding lets a player perform an action on a grid spot while moving
-            tileController.TestSwap(new Vector3Int(playerNearGridX, playerNearGridY, 0));
+            int playerNearGridY = (int)Math.Round(transform.position.y - 1.28f); // this rounding lets a player perform an action on a grid spot while moving
+
+            //plant a plant
+            tileController.Plant(new Vector3Int(playerNearGridX, playerNearGridY, 0), instrumentSelection);
+        }
+        if(Input.GetKeyDown(KeyCode.R)){
+            int playerNearGridX = (int)Math.Round(transform.position.x - 0.5f); // the -0.5f is because the sprite position is determined by its center, whereas the tilemap tiles are determined by their lower left corner
+            int playerNearGridY = (int)Math.Round(transform.position.y - 1.28f); // this rounding lets a player perform an action on a grid spot while moving
+
+            //uproot a plant
+            tileController.Uproot(new Vector3Int(playerNearGridX, playerNearGridY, 0));
         }
 
     }
@@ -44,6 +74,41 @@ public class PlayerController : MonoBehaviour
         targetPos = origPos + direction;
 
         while(elapsedTime < timeToMove){
+            if (direction == Vector3.down) { // animation
+                if(elapsedTime > 0.25f * timeToMove && elapsedTime < 0.75f * timeToMove){
+                    if(rightFootNext)
+                        spriteRenderer.sprite = spriteArray[1];
+                    else
+                        spriteRenderer.sprite = spriteArray[3];
+                    
+                }
+                else
+                    spriteRenderer.sprite = spriteArray[0];
+            }
+            if (direction == Vector3.up) { // animation
+                if(elapsedTime > 0.25f * timeToMove && elapsedTime < 0.75f * timeToMove){
+                    if(rightFootNext)
+                        spriteRenderer.sprite = spriteArray[7];
+                    else
+                        spriteRenderer.sprite = spriteArray[5];
+                    
+                }
+                else
+                    spriteRenderer.sprite = spriteArray[4];
+            }
+            if (direction == Vector3.left) { // animation
+                if(elapsedTime > 0.25f * timeToMove && elapsedTime < 0.75f * timeToMove)
+                        spriteRenderer.sprite = spriteArray[9];
+                else
+                    spriteRenderer.sprite = spriteArray[8];
+            }
+            if (direction == Vector3.right) { // animation
+                if(elapsedTime > 0.25f * timeToMove && elapsedTime < 0.75f * timeToMove)
+                        spriteRenderer.sprite = spriteArray[11];
+                else
+                    spriteRenderer.sprite = spriteArray[10];
+            }
+
             transform.position = Vector3.Lerp(origPos, targetPos, elapsedTime / timeToMove);
             elapsedTime += Time.deltaTime;
             yield return null;
@@ -52,6 +117,7 @@ public class PlayerController : MonoBehaviour
         transform.position = targetPos; // this prevents any accumulation of imperfect movements
 
         isMoving = false;
+        rightFootNext = !rightFootNext;
     }
 
     private void PlayFootstepSound(Vector3 direction)
--- Rhythm Jam/Assets/Scripts/TileController.cs
+++ Rhythm Jam/Assets/Scripts/TileController.cs
@@ -5,21 +5,33 @@ using UnityEngine.Tilemaps;
 
 public class TileController : MonoBehaviour
 {
-    private Tilemap tilemap;
+    public Tilemap[] tilemaps;
     public TileBase grass;
     public TileBase plantspot;
+
+    public TileBase bassNew;
+    public TileBase bassGrown;
+    public TileBase drumkitNew;
+    public TileBase drumkitGrown;
+    public TileBase guitarNew;
+    public TileBase guitarGrown;
+    public TileBase pianoNew;
+    public TileBase pianoGrown;
+    public TileBase pipeorganNew;
+    public TileBase pipeorganGrown;
+
     public int gridHeight = 9;
     public int gridLength = 13;
 
     // Start is called before the first frame update
     void Start()
     {
-        tilemap = gameObject.GetComponent<Tilemap>();
+        // tilemap = gameObject.GetComponent<Tilemap>();
     }
 
     // returns the name of the tile's sprite at this position, e.g. "grass"
     public string GetTileName(Vector3Int pos){
-        if(tilemap == null){
+        if(tilemaps[pos.y] == null){
             print("tilemap not found");
             return null;
         } 
@@ -28,7 +40,11 @@ public class TileController : MonoBehaviour
             return null;
         }
         
-        return tilemap.GetTile<Tile>(pos).name;
+        Tile tile = tilemaps[pos.y].GetTile<Tile>(pos);
+        if(tile == null)
+            return null;
+
+        return tile.name;
     }
 
     // toggles a spot. This is only meant for testing purposes
@@ -37,8 +53,114 @@ public class TileController : MonoBehaviour
         if(name == null)
             return;
         if(name == "grass"){
-            tilemap.SetTile(pos, plantspot);
+            tilemaps[0].SetTile(pos, plantspot);
         }
-        else tilemap.SetTile(pos, grass);
+        else tilemaps[0].SetTile(pos, grass);
+    }
+
+    // plants a type of plant (instrument) at position pos
+    public void Plant(Vector3Int pos, InstrumentEnum.Instrument instrument){
+        TileBase selectedPlant = drumkitGrown;
+        Uproot(pos);
+
+        switch (instrument)
+        {
+            case InstrumentEnum.Instrument.Piano:
+                selectedPlant = pianoNew;
+                break;
+            case InstrumentEnum.Instrument.Organ:
+                selectedPlant = pipeorganNew;
+                break;
+            case InstrumentEnum.Instrument.Guitar:
+                selectedPlant = guitarNew;
+                break;
+            case InstrumentEnum.Instrument.Drums:
+                selectedPlant = drumkitNew;
+                break;
+            case InstrumentEnum.Instrument.Bass:
+                selectedPlant = bassNew;
+                break;
+        }
+
+        if(pos.x > 3 && pos.x < 12){
+            if(pos.y == 1 || pos.y == 2 || pos.y == 6 || pos.y == 7){
+                tilemaps[pos.y].SetTile(pos, selectedPlant);
+                MusicHandler.Instance.AddPlant(pos.x - 3, false, instrument);
+            }
+        }else if(pos.x == 1){
+            if(pos.y == 3 || pos.y == 4 || pos.y == 5){
+                tilemaps[pos.y].SetTile(pos, selectedPlant);
+                MusicHandler.Instance.AddPlant(9, false, instrument);
+            }
+        }
+    }
+
+    // removes the plant at position pos if one is there
+    public void Uproot(Vector3Int pos){
+        if(pos.x > 3 && pos.x < 12){
+            if(pos.y == 1 || pos.y == 2 || pos.y == 6 || pos.y == 7){
+                InstrumentEnum.Instrument instrument = InstrumentEnum.Instrument.Piano;
+                switch (GetTileName(pos))
+                {
+                    case "PianoNew":
+                    case "PianoGrown":
+                        instrument = InstrumentEnum.Instrument.Piano;
+                        break;
+                    case "PipeorganNew":
+                    case "PipeorganGrown":
+                        instrument = InstrumentEnum.Instrument.Organ;
+                        break;
+                    case "GuitarNew":
+                    case "GuitarGrown":
+                        instrument = InstrumentEnum.Instrument.Guitar;
+                        break;
+                    case "DrumkitNew":
+                    case "DrumkitGrown":
+                        instrument = InstrumentEnum.Instrument.Drums;
+                        break;
+                    case "BassNew":
+                    case "BassGrown":
+                        instrument = InstrumentEnum.Instrument.Bass;
+                        break;
+                    default:
+                        return; // in this case there is nothing planted on this spot
+                }
+                tilemaps[pos.y].SetTile(pos, null);
+                MusicHandler.Instance.RemovePlant(pos.x - 3, instrument);
+            }
+        }
+
+        else if(pos.x == 1){
+            if(pos.y == 3 || pos.y == 4 || pos.y == 5){
+                InstrumentEnum.Instrument instrument = InstrumentEnum.Instrument.Piano;
+                switch (GetTileName(pos))
+                {
+                    case "PianoNew":
+                    case "PianoGrown":
+                        instrument = InstrumentEnum.Instrument.Piano;
+                        break;
+                    case "PipeorganNew":
+                    case "PipeorganGrown":
+                        instrument = InstrumentEnum.Instrument.Organ;
+                        break;
+                    case "GuitarNew":
+                    case "GuitarGrown":
+                        instrument = InstrumentEnum.Instrument.Guitar;
+                        break;
+                    case "DrumkitNew":
+                    case "DrumkitGrown":
+                        instrument = InstrumentEnum.Instrument.Drums;
+                        break;
+                    case "BassNew":
+                    case "BassGrown":
+                        instrument = InstrumentEnum.Instrument.Bass;
+                        break;
+                    default:
+                        return; // in this case there is nothing planted on this spot
+                }
+                tilemaps[pos.y].SetTile(pos, null);
+                MusicHandler.Instance.RemovePlant(9, instrument);
+            }
+        }
     }
 }
